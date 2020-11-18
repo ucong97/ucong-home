7b211d800737cdb8f0daef6de0447495620205cd
ucong2222 <dbrudrjf21@gmail.com>
2020-11-18 09:55:07 +0900

    getMemberById::MysqlUtil 적용

--- src/com/sbs/example/ucong/dao/MemberDao.java
+++ src/com/sbs/example/ucong/dao/MemberDao.java
@@ -1,10 +1,5 @@
 package com.sbs.example.ucong.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Map;
 
 import com.sbs.example.mysqlutil.MysqlUtil;
@@ -41,60 +36,19 @@ public class MemberDao {
 		return new Member(memberMap);
 	}
 
-	public Member getMemberById(int memberId) {
-		Connection con = null;
-		Member member=null;
-		try {
-			String url = "jdbc:mysql://localhost:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60000&socketTimeout=60000";
-			String user = "sbsst";
-			String pass = "sbs123414";
-			// 드라이버 등록
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// 연결생성
-			try {
-				con = DriverManager.getConnection(url, user, pass);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			String sql = "SELECT * FROM member WHERE Id = ?";
-			
-			try {
-				PreparedStatement ps = con.prepareStatement(sql);
-				
-				ps.setInt(1,memberId);
-				
-				ResultSet rs = ps.executeQuery();
-				
-				if(rs.next()) {
-					int id= rs.getInt("id");
-					String memberLoginId=rs.getString("loginId");
-					String memberLoginPw=rs.getString("loginPw");
-					String memberName=rs.getString("name");
-					
-					member = new Member(id,memberLoginId,memberLoginPw,memberName);
-				}
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-		} finally {
-			try {
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	public Member getMemberById(int id) {
+		SecSql sql = new SecSql();
+		sql.append("SELECT *");
+		sql.append("FROM member");
+		sql.append("WHERE Id = ?",id);
+		
+		Map<String,Object> memberMap = MysqlUtil.selectRow(sql);
+		
+		if(memberMap.isEmpty()) {
+			return null;
 		}
-		return member;
+		
+		return new Member(memberMap);
 	}
 
 }
