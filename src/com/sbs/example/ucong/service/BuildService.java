dc757c19618cb5ba12e09f198936259b081e925c
ucong2222 <dbrudrjf21@gmail.com>
2020-12-09 19:22:03 +0900

    StringBuilder 사용

--- src/com/sbs/example/ucong/service/BuildService.java
+++ src/com/sbs/example/ucong/service/BuildService.java
@@ -9,77 +9,83 @@ import com.sbs.example.ucong.util.Util;
 
 public class BuildService {
 	private ArticleService articleService;
-	
+
 	public BuildService() {
-		articleService= Container.articleService;
+		articleService = Container.articleService;
 	}
-	
+
 	public void buildSite() {
 		System.out.println("site 폴더생성");
 		Util.rmdir("site");
 		Util.mkdirs("site");
-		
-		Util.copy("site_template/part/app.css","site/app.css");
-		
+
+		Util.copy("site_template/part/app.css", "site/app.css");
+
 		String head = Util.getFileContents("site_template/part/head.html");
 		String foot = Util.getFileContents("site_template/part/foot.html");
-		
+
 		// 각 게시판 별 게시물 리스트 페이지 생성
 		List<Board> boards = articleService.getBoards();
-		
-		for(Board board: boards) {
-			String html="";
-			String fileName= board.code + "_article_list_1.html";
-			String filePath= "site/" + fileName;
+
+		for (Board board : boards) {
+			StringBuilder sb = new StringBuilder();
+			String fileName = board.code + "_article_list_1.html";
+			String filePath = "site/" + fileName;
 			List<Article> articles = articleService.getArticlesByBoardId(board.id);
-			for(Article article:articles) {
-				html+= "<div>번호 : " + article.id + "</div>";
-				html+= "<div>작성일 : " + article.regDate + "</div>";
-				html+= "<div><a href=\""+article.id +".html\">제목 : " + article.title + "</a></div>";
-			}
-			html = head + html + foot;
+			for (Article article : articles) {
+				sb.append("<div>번호 : " + article.id + "</div>");
+				sb.append("<div>작성일 : " + article.regDate + "</div>");
+				sb.append("<div><a href=" + article.id + ".html>제목 :" + article.title + "</a></div>");
 
-			Util.writeFileContents(filePath, html);
+			}
+
+			Util.writeFileContents(filePath, head + sb.toString() + foot);
 		}
-		
-		//게시물 별 파일생성생성
+		System.out.println("각 게시판 별 게시물 리스트 페이지 생성");
+
+		// 게시물 별 파일생성
 		List<Article> articles = articleService.getArticles();
-		for(Article article : articles) {
-			
-			String html = "";
-			String fileName=article.id+".html";
-			String filePath= "site/" + fileName;
-			html += "<div>번호 : " + article.id + "</div>";
-			html += "<div>날짜 : " + article.regDate + "</div>";
-			html += "<div>제목 : " + article.title + "</div>";
-			html += "<div>내용 : " + article.body + "</div>";
+		for (Article article : articles) {
+			StringBuilder sb = new StringBuilder();
 
-			if(article.id>1) {
-				html+= "<div><a href=\""+(article.id-1)+".html\">이전글</a></div>";
+			String fileName = article.id + ".html";
+			String filePath = "site/" + fileName;
+
+			sb.append("<div>");
+			sb.append("번호 : " + article.id + "<br>");
+			sb.append("작성날짜 : " + article.regDate + "<br>");
+			sb.append("갱신날짜 : " + article.updateDate + "<br>");
+			sb.append("제목 : " + article.title + "<br>");
+			sb.append("내용 : " + article.body + "<br>");
+
+			if (article.id > 1) {
+				sb.append("<a href=\"" + (article.id - 1) + ".html\">이전글</a>");
 			}
-			html += "<div><a href=\""+(article.id+1)+".html\">다음글</a></div>";
+			if (article.id < articles.size()) {
+				sb.append("<a href=\"" + (article.id + 1) + ".html\">다음글</a>");
+			}
+			sb.append("</div>");
+			Util.writeFileContents(filePath, head + sb.toString() + foot);
+		}
+		System.out.println("게시물 별 파일생성");
+		
+		// 모든 게시물 리스트 페이지 생성
+		String fileName = "article_list.html";
+		String filePath = "site/" + fileName;
+		StringBuilder sb = new StringBuilder();
+		for (Article article : articles) {
+			sb.append("<div>번호 : " + article.id + " 제목 :<a href=\"" + article.id + ".html\"> " + article.title
+					+ "</a></div>");
+		}
+		Util.writeFileContents(filePath,head + sb.toString() + foot);
+		System.out.println("리스트 페이지 생성");
 
-			html = head + html+ foot;
-			Util.writeFileContents(filePath,html);
-		}
-		//모든 게시물 리스트 페이지 생성
-		String html = "";
-		String fileName="article_list.html";
-		String filePath= "site/" + fileName;
-		for(Article article : articles) {
-			
-			html+="<div>번호 : " + article.id + " 제목 :<a href=\""+article.id+".html\"> "+article.title+"</a></div>";
-		}
-		
-		html = head + html+ foot;
-		Util.writeFileContents(filePath,html);
-		
-		//인덱스 생성
-		html = "";
-		fileName="index.html";
-		filePath= "site/" + fileName;
-		html = head + html+ foot;
-		Util.writeFileContents(filePath,html);
+		// 인덱스 생성
+		sb.setLength(0); 
+		fileName = "index.html";
+		filePath = "site/" + fileName;
+		Util.writeFileContents(filePath, head + sb.toString() + foot);
+		System.out.println("인덱스 생성");
 	}
 
 }
