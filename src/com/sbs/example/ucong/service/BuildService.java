2613ae62f48c2b766f2ed985a5c9322e83a05679
ucong2222 <dbrudrjf21@gmail.com>
2020-12-11 11:41:29 +0900

    타이틀바 콘텐츠 반환 함수 만들어 전체페이지 생성에 적용

--- src/com/sbs/example/ucong/service/BuildService.java
+++ src/com/sbs/example/ucong/service/BuildService.java
@@ -18,16 +18,20 @@ public class BuildService {
 		System.out.println("site 폴더생성");
 		Util.rmdir("site");
 		Util.mkdirs("site");
-
+		
 		Util.copy("site_template/part/app.css", "site/app.css");
+		
+		buildIndexPage();
+		buildArticleDetailPages();
+		buildBoardArticlePages();
 
-		String head = getHeadHtml();
-		String foot = Util.getFileContents("site_template/part/foot.html");
-
+	}
+	private void buildBoardArticlePages() {
 		List<Board> boards = articleService.getBoards();
-
+		String foot = Util.getFileContents("site_template/part/foot.html");
 		// 각 게시판 별 게시물 리스트 페이지 생성
 		for (Board board : boards) {
+			String head = getHeadHtml("article_list_"+board.code);
 			StringBuilder sb = new StringBuilder();
 			String fileName = board.code + "_article_list_1.html";
 			String filePath = "site/" + fileName;
@@ -42,8 +46,12 @@ public class BuildService {
 			Util.writeFileContents(filePath, head + sb.toString() + foot);
 		}
 		System.out.println("각 게시판 별 게시물 리스트 페이지 생성");
-		// 게시물 별 파일생성
+	}
+
+	private void buildArticleDetailPages() {
 		List<Article> articles = articleService.getArticles();
+		String head = getHeadHtml("article_detail");
+		String foot = Util.getFileContents("site_template/part/foot.html");
 		for (Article article : articles) {
 			StringBuilder sb = new StringBuilder();
 
@@ -68,26 +76,27 @@ public class BuildService {
 		}
 		System.out.println("게시물 별 파일생성");
 
-		// 모든 게시물 리스트 페이지 생성
-		String fileName = "article_list.html";
-		String filePath = "site/" + fileName;
-		StringBuilder sb = new StringBuilder();
-		for (Article article : articles) {
-			sb.append("<div>번호 : " + article.id + " 제목 :<a href=\"" + article.id + ".html\"> " + article.title
-					+ "</a></div>");
-		}
-		Util.writeFileContents(filePath, head + sb.toString() + foot);
-		System.out.println("리스트 페이지 생성");
-
-		// 인덱스 생성
-		sb.setLength(0);
-		fileName = "index.html";
-		filePath = "site/" + fileName;
-		Util.writeFileContents(filePath, head + sb.toString() + foot);
-		System.out.println("인덱스 생성");
 	}
+
+	private void buildIndexPage() {
+		StringBuilder sb = new StringBuilder();
+		
+		String head = getHeadHtml("index");
+		String foot = Util.getFileContents("site_template/part/foot.html");
+	
+		String mainHtml = Util.getFileContents("site_template/part/index.html");
+		
+		sb.append(head);
+		sb.append(mainHtml);
+		sb.append(foot);
+		
+		String filePath = "site/index.html";
+		Util.writeFile(filePath, sb.toString());
+		System.out.println(filePath+ "생성");
+	}
+
 	// 헤더 게시판 메뉴 동적생성
-	private String getHeadHtml() {
+	private String getHeadHtml(String pageName) {
 		String head = Util.getFileContents("site_template/part/head.html");
 
 		StringBuilder boardMenuContentHtml = new StringBuilder();
@@ -98,25 +107,30 @@ public class BuildService {
 			boardMenuContentHtml.append("<li>");
 			boardMenuContentHtml
 					.append("<a href=\"" + board.code + "_article_list_1.html\" class=\"block text-align-center\">");
-			String iClass = "fas fa-clipboard-list";
-
-			if (board.code.contains("notice")) {
-				iClass = "fab fa-free-code-camp";
-			} else if (board.code.contains("free")) {
-				iClass = "fab fa-free-code-camp";
-			}
-			boardMenuContentHtml.append("<i class=\"" + iClass + "\"></i>");
-
-			boardMenuContentHtml.append(" ");
-			boardMenuContentHtml.append("<span>");
-			boardMenuContentHtml.append(board.code);
-			boardMenuContentHtml.append("</span>");
+			
+			boardMenuContentHtml.append(getTitleBarContentByPageName("article_list_"+board.code));
 			boardMenuContentHtml.append("</a>");
 			boardMenuContentHtml.append("</li>");
 		}
 		head = head.replace("${menu-bar__menu-1__board-menu-content}", boardMenuContentHtml.toString());
-
+		String titleBarContentHtml = getTitleBarContentByPageName(pageName);
+		head = head.replace("${title-bar__content}", titleBarContentHtml);
 		return head;
+	}
+
+	private String getTitleBarContentByPageName(String pageName) {
+		if(pageName.equals("index")) {
+			return "<i class=\"fas fa-home\"></i> <span>HOME</span>";
+		}else if (pageName.equals("article_detail")) {
+			return "<i class=\"fas fa-file-alt\"></i> <span>ARTICLE DETAIL</span>";
+		} else if (pageName.startsWith("article_list_free")) {
+			return "<i class=\"fab fa-free-code-camp\"></i> <span>FREE LIST</span>";
+		} else if (pageName.startsWith("article_list_notice")) {
+			return "<i class=\"fas fa-flag\"></i> <span>NOTICE LIST</span>";
+		} else if (pageName.startsWith("article_list_")) {
+			return "<i class=\"fas fa-clipboard-list\"></i> <span>NOTICE LIST</span>";
+		}
+		return "";
 	}
 
 }
