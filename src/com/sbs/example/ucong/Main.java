d7224f79cddfbf00720d4b83bdc667fa4378976e
ucong2222 <dbrudrjf21@gmail.com>
2020-12-30 16:40:33 +0900

    HTTP REQUEST 후 응답 받아오는 함수 구현

--- src/com/sbs/example/ucong/Main.java
+++ src/com/sbs/example/ucong/Main.java
@@ -1,6 +1,17 @@
 package com.sbs.example.ucong;
+
+import com.sbs.example.ucong.util.Util;
+
 public class Main {
 	public static void main(String[] args) {
-		new App().run();
+		testApi();
+		//new App().run();
+	}
+
+	private static void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		String rs = Util.callApi(url,"api_key=AYoqRgidP7DfQxNRxRiJ11Ciqq3iL3GdXUqKdjvsZdZzbLrADjPoBRmOWrYB0Wrt","forum=cong-ssg","thread:ident=article_detail_1.html");
+		
+		System.out.println(rs);
 	}
 }
--- src/com/sbs/example/ucong/util/Util.java
+++ src/com/sbs/example/ucong/util/Util.java
@@ -1,13 +1,18 @@
 package com.sbs.example.ucong.util;
 
-
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,11 +20,10 @@ import java.nio.file.StandardCopyOption;
 
 public class Util {
 
-
 	public static void mkdirs(String path) {
 		File dir = new File(path);
-		
-		if(dir.exists()==false) {
+
+		if (dir.exists() == false) {
 			dir.mkdirs();
 		}
 	}
@@ -91,26 +95,78 @@ public class Util {
 
 	public static boolean copy(String sourcePath, String destPath) {
 		Path source = Paths.get(sourcePath);
-        Path target = Paths.get(destPath);
-        
-        if (!Files.exists(target.getParent())) {
-            try {
+		Path target = Paths.get(destPath);
+
+		if (!Files.exists(target.getParent())) {
+			try {
 				Files.createDirectories(target.getParent());
 			} catch (IOException e) {
 				e.printStackTrace();
 				return false;
 			}
-        
-        }
-        try {
+
+		}
+		try {
 			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
-        
+
 			return true;
 		}
 
-        return true;
+		return true;
+	}
+
+	public static String callApi(String urlStr, String... args) {
+		// URL 구성 시작
+		StringBuilder queryString = new StringBuilder();
+
+		for (String param : args) {
+			if (queryString.length() == 0) {
+				queryString.append("?");
+			} else {
+				queryString.append("&");
+			}
+
+			queryString.append(param);
+		}
+
+		urlStr += queryString.toString();
+		// URL 구성 끝
+
+		// 연결생성 시작
+		HttpURLConnection con = null;
+
+		try {
+			URL url = new URL(urlStr);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setConnectTimeout(5000); // 최대통신시간 제한
+			con.setReadTimeout(5000); // 최대데이터읽기시간 제한
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		// 연결생성 끝
+
+		// 연결을 통해서 데이터 가져오기 시작
+		StringBuffer content = null;
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+			String inputLine;
+			content = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// 연결을 통해서 데이터 가져오기 끝
+
+		return content.toString();
 	}
 }
-
-
