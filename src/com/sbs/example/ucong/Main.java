fa4800bbdcdda06f7631a79620a1f3745081d200
ucong2222 <dbrudrjf21@gmail.com>
2020-12-30 17:27:37 +0900

    잭슨 라이브러리 추가 및 테스트

--- src/com/sbs/example/ucong/Main.java
+++ src/com/sbs/example/ucong/Main.java
@@ -1,17 +1,13 @@
 package com.sbs.example.ucong;
 
-import com.sbs.example.ucong.util.Util;
+import com.sbs.example.ucong.test.TestRunner;
 
 public class Main {
 	public static void main(String[] args) {
-		testApi();
-		//new App().run();
+		
+		new TestRunner().run();
+		// new App().run();
 	}
 
-	private static void testApi() {
-		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
-		String rs = Util.callApi(url,"api_key=AYoqRgidP7DfQxNRxRiJ11Ciqq3iL3GdXUqKdjvsZdZzbLrADjPoBRmOWrYB0Wrt","forum=cong-ssg","thread:ident=article_detail_1.html");
-		
-		System.out.println(rs);
-	}
+	
 }
---
+++ src/com/sbs/example/ucong/test/TestRunner.java
@@ -0,0 +1,123 @@
+package com.sbs.example.ucong.test;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sbs.example.ucong.util.Util;
+
+public class TestRunner {
+	public static class TestDAtaType1{
+		@JsonIgnoreProperties(ignoreUnknown = true)
+		
+		public int age;
+		public String name;
+		public int height;
+	}
+	public void run() {
+		// testApi();
+		// testJackson();
+		// testJackson2();
+		// testJackson3();
+		//testJackson4();
+		testJackson5();
+	}
+
+
+	private void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		String rs = Util.callApi(url, "api_key=AYoqRgidP7DfQxNRxRiJ11Ciqq3iL3GdXUqKdjvsZdZzbLrADjPoBRmOWrYB0Wrt",
+				"forum=cong-ssg", "thread:ident=article_detail_1.html");
+
+		System.out.println(rs);
+	}
+
+	private void testJackson() {
+		String jsonString = "{\"age\":22, \"name\":\"홍길동\"}";
+
+		ObjectMapper ob = new ObjectMapper();
+		Map rs = null;
+
+		try {
+			rs = ob.readValue(jsonString, Map.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs);
+		System.out.println(rs.get("age"));
+
+	}
+
+	private void testJackson2() {
+		String jsonString = "1";
+
+		ObjectMapper ob = new ObjectMapper();
+		Integer rs = null;
+
+		try {
+			rs = ob.readValue(jsonString, Integer.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs);
+	}
+
+	private void testJackson3() {
+		String jsonString = "[1,2,3]";
+
+		ObjectMapper ob = new ObjectMapper();
+		List<Integer> rs = null;
+
+		try {
+			rs = ob.readValue(jsonString, List.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs);
+		System.out.println(rs.get(1));
+	}
+
+	private void testJackson4() {
+		String jsonString = "[{\"age\":22, \"name\":\"홍길동\"},{\"age\":25, \"name\":\"홍길순\"},{\"age\":20, \"name\":\"홍길삼\"}]";
+
+		ObjectMapper ob = new ObjectMapper();
+		List<Map<String,Object>> rs = null;
+
+		try {
+			rs = ob.readValue(jsonString, List.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs);
+		System.out.println(rs.get(1));
+		System.out.println(rs.get(1).get("age"));
+	}
+	
+	private void testJackson5() {
+		String jsonString = "[{\"age\":22, \"name\":\"홍길동\", \"height\":178},{\"age\":23, \"name\":\"홍길순\", \"height\":168},{\"age\":24, \"name\":\"임꺽정\"}]";
+
+		ObjectMapper ob = new ObjectMapper();
+		List<TestDAtaType1> rs = null;
+		
+		
+		try {
+			rs = ob.readValue(jsonString, new TypeReference<List<TestDAtaType1>>(){});
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		System.out.println(rs.get(0).height + rs.get(2).height);
+	}
+}
